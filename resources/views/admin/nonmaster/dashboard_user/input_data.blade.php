9c883e9dc7d13f75bbe42ba08d7a9d9146cfb677
DAN <danang@NCC.com>
2017-02-20 21:18:38 +0700

    edit view input_data

--- resources/views/admin/nonmaster/dashboard_user/input_data.blade.php
+++ resources/views/admin/nonmaster/dashboard_user/input_data.blade.php
@@ -6,77 +6,204 @@
 @include('admin.master.navbar')
     <div class="main-panel">
 @include('admin.master.top_navbar', ['navbartitle' => 'Input Data'])
-                <div class="content">
+        <div class="content">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-md-8">
-                        <div class="card">
-                            <div class="header">
-                                <h4 class="title">Input Listrik</h4>
-                            </div>
-                            <div class="content">
-                                <form>
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <div class="form-group">
-                                                <label>Stand Akhir</label>
-                                                <input type="text" class="form-control" placeholder="StandAkhir" value="">
+                        
+                    <form>
+                        <div class="col-md-8">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="card">
+                                        <div class="header">
+                                            <h4 class="title">LWBP1</h4>
+                                        </div>
+                                        <div class="content">
+                                            
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label>Stand Akhir</label>
+                                                        <input type="text" class="form-control" placeholder="LWBP1tandAkhir" value="">
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label>Faktor Kali</label>
+                                                        <input type="text" class="form-control" placeholder="LWBP1StandAkhir" value="">
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
+                                </div>
 
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <div class="form-group">
-                                                <label>Faktor Kali</label>
-                                                <input type="text" class="form-control" placeholder="StandAkhir" value="">
+
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="card">
+                                        <div class="header">
+                                            <h4 class="title">LWBP2</h4>
+                                        </div>
+                                        <div class="content">
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label>Stand Akhir</label>
+                                                        <input type="text" class="form-control" placeholder="LWBP2StandAkhir" value="">
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label>Faktor Kali</label>
+                                                        <input type="text" class="form-control" placeholder="LWBP2StandAkhir" value="">
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="card">
+                                        <div class="header">
+                                            <h4 class="title">WBP</h4>
+                                        </div>
+                                        <div class="content">
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label>Stand Akhir</label>
+                                                        <input type="text" class="form-control" placeholder="WBPStandAkhir" value="">
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label>Faktor Kali</label>
+                                                        <input type="text" class="form-control" placeholder="WBPStandAkhir" value="">
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="card">
+                                        <div class="header">
+                                            <h4 class="title">KVARH</h4>
+                                        </div>
+                                        <div class="content">
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label>Stand Akhir</label>
+                                                        <input type="text" class="form-control" placeholder="KVARHStandAkhir" value="">
+                                                    </div>
+                                                </div>
+                                            </div>
 
-                                    <button type="submit" class="btn btn-info btn-fill pull-right">Input Listrik</button>
-                                    <div class="clearfix"></div>
-                                </form>
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label>Faktor Kali</label>
+                                                        <input type="text" class="form-control" placeholder="KVARHStandAkhir" value="">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="card">
+                                        <div class="header">
+                                            <h4 class="title">KW</h4>
+                                        </div>
+                                        <div class="content">
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label>Stand Akhir</label>
+                                                        <input type="text" class="form-control" placeholder="KWStandAkhir" value="">
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label>Faktor Kali</label>
+                                                        <input type="text" class="form-control" placeholder="KWStandAkhir" value="">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="row">
+                                    <!-- Profil -->
+                                <div class="col-md-12">
+                                    <div class="card card-user">
+                                        <div class="image">
+                                            <img src="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400" alt="..."/>
+                                        </div>
+                                        <div class="content">
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label>Nama GI / GD</label>
+                                                        <input type="text" class="form-control" disabled placeholder="Company" value="GI 147">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label>Area</label>
+                                                        <input type="text" class="form-control" disabled placeholder="Company" value="Surabaya">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label>Rayon</label>
+                                                        <input type="text" class="form-control" disabled placeholder="Company" value="Sukolilo">
+                                                    </div>
+                                                </div>
+                                            </div>
+
+
+                                            <button type="submit" class="btn btn-info btn-fill pull-right">Input Listrik</button>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                        <hr>
+
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-4">
-                        <div class="card card-user">
-                            <div class="image">
-                                <img src="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400" alt="..."/>
-                            </div>
-                            <div class="content">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label>Nama GI / GD</label>
-                                            <input type="text" class="form-control" disabled placeholder="Company" value="GI 147">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label>Area</label>
-                                            <input type="text" class="form-control" disabled placeholder="Company" value="Surabaya">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label>Rayon</label>
-                                            <input type="text" class="form-control" disabled placeholder="Company" value="Sukolilo">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <hr>
-
-                        </div>
-                    </div>
-
-                </div>
+                </form>
             </div>
         </div>
 
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit2d8ddd5c4ffa94bca61c12f0bfaf53ee
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit2d8ddd5c4ffa94bca61c12f0bfaf53ee', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
