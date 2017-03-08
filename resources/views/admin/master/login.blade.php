c86abd08262b0f79539d0a77fa666344cdcefc5e
Danang Adi Nugroho <danang.adi.n@outlook.com>
2017-03-08 16:50:50 +0700

    edit login, delete material template

--- resources/views/admin/master/login.blade.php
+++ resources/views/admin/master/login.blade.php
@@ -2,39 +2,130 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
-	<link rel="apple-touch-icon" sizes="76x76" href="{{ URL::asset('material-kit/img/apple-icon.png') }}">
-	<link rel="icon" type="image/png" href="{{ URL::asset('material-kit/img/favicon.png') }}">
+	<link rel="icon" type="image/png" href="http://demos.creative-tim.com/light-bootstrap-dashboard-pro/assets/img/favicon.ico">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-	<title>SI - ONENG | Login</title>
+	<title>Light Bootstrap Dashboard PRO by Creative Tim</title>
 
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+	<meta name="viewport" content="width=device-width" />
+
+
+	<!-- Canonical SEO -->
+	<link rel="canonical" href="https://www.creative-tim.com/product/light-bootstrap-dashboard-pro"/>
+
+	<!--  Social tags      -->
+	<meta name="keywords" content="creative tim, html dashboard, html css dashboard, web dashboard, bootstrap dashboard, bootstrap, css3 dashboard, bootstrap admin, light bootstrap dashboard, frontend, responsive bootstrap dashboard">
+
+	<meta name="description" content="Forget about boring dashboards, get an admin template designed to be simple and beautiful.">
+
+	<!-- Schema.org markup for Google+ -->
+	<meta itemprop="name" content="Light Bootstrap Dashboard PRO by Creative Tim">
+	<meta itemprop="description" content="Forget about boring dashboards, get an admin template designed to be simple and beautiful.">
+
+	<meta itemprop="image" content="http://s3.amazonaws.com/creativetim_bucket/products/34/original/opt_lbd_pro_thumbnail.jpg">
+	<!-- Twitter Card data -->
+
+	<meta name="twitter:card" content="product">
+	<meta name="twitter:site" content="@creativetim">
+	<meta name="twitter:title" content="Light Bootstrap Dashboard PRO by Creative Tim">
+
+	<meta name="twitter:description" content="Forget about boring dashboards, get an admin template designed to be simple and beautiful.">
+	<meta name="twitter:creator" content="@creativetim">
+	<meta name="twitter:image" content="http://s3.amazonaws.com/creativetim_bucket/products/34/original/opt_lbd_pro_thumbnail.jpg">
+	<meta name="twitter:data1" content="Light Bootstrap Dashboard PRO by Creative Tim">
+	<meta name="twitter:label1" content="Product Type">
+	<meta name="twitter:data2" content="$29">
+	<meta name="twitter:label2" content="Price">
+
+	<!-- Open Graph data -->
+	<meta property="og:title" content="Light Bootstrap Dashboard PRO by Creative Tim" />
+	<meta property="og:type" content="article" />
+	<meta property="og:url" content="{{url('/')}}" />
+	<meta property="og:image" content="http://s3.amazonaws.com/creativetim_bucket/products/34/original/opt_lbd_pro_thumbnail.jpg"/>
+	<meta property="og:description" content="Forget about boring dashboards, get an admin template designed to be simple and beautiful." />
+	<meta property="og:site_name" content="Creative Tim" />
+
+
+	<!-- Bootstrap core CSS     -->
+	<link href="{{ URL::asset('dashboard/css/bootstrap.min.css') }}" rel="stylesheet" />
+
+	<!--  Light Bootstrap Dashboard core CSS    -->
+	<link href="{{ URL::asset('dashboard/css/light-bootstrap-dashboard.css') }}" rel="stylesheet"/>
+
+	<!--  CSS for Demo Purpose, don't include it in your project     -->
+	{{--<link href="../../assets/css/demo.css" rel="stylesheet" />--}}
+
 
 	<!--     Fonts and icons     -->
-	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" />
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
-
-	<!-- CSS Files -->
-    <link href="{{ URL::asset('material-kit/css/bootstrap.min.css') }}" rel="stylesheet" />
-    <link href="{{ URL::asset('material-kit/css/material-kit.css') }}" rel="stylesheet"/>
+	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+	<link href="{{ URL::asset('dashboard/css/pe-icon-7-stroke.css') }}" rel="stylesheet" />
 
 </head>
 
 @yield('content')
-    
-	<!--   Core JS Files   -->
-	<script src="{{ URL::asset('material-kit/js/jquery.min.js') }}" type="text/javascript"></script>
-	<script src="{{ URL::asset('material-kit/js/bootstrap.min.js') }}" type="text/javascript"></script>
-	<script src="{{ URL::asset('material-kit/js/material.min.js') }}"></script>
 
-	<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-	<script src="{{ URL::asset('material-kit/js/nouislider.min.js') }}" type="text/javascript"></script>
+	<!--   Core JS Files and PerfectScrollbar library inside jquery.ui   -->
+	<script src="{{ URL::asset('dashboard/js/jquery.min.js') }}" type="text/javascript"></script>
+	<script src="{{ URL::asset('dashboard/js/jquery-ui.min.js') }}" type="text/javascript"></script>
+	<script src="{{ URL::asset('dashboard/js/bootstrap.min.js') }}" type="text/javascript"></script>
 
-	<!--  Plugin for the Datepicker, full documentation here: http://www.eyecon.ro/bootstrap-datepicker/ -->
-	<script src="{{ URL::asset('material-kit/js/bootstrap-datepicker.js') }}" type="text/javascript"></script>
+	<!--  Forms Validations Plugin -->
+	<script src="{{ URL::asset('dashboard/js/jquery.validate.min.js') }}"></script>
 
-	<!-- Control Center for Material Kit: activating the ripples, parallax effects, scripts from the example pages etc -->
-	<script src="{{ URL::asset('material-kit/js/material-kit.js') }}" type="text/javascript"></script>
+	<!--  Plugin for Date Time Picker and Full Calendar Plugin-->
+	<script src="{{ URL::asset('dashboard/js/moment.min.js') }}"></script>
 
+	<!--  Date Time Picker Plugin is included in this js file -->
+	<script src="{{ URL::asset('dashboard/js/bootstrap-datetimepicker.js') }}"></script>
+
+	<!--  Select Picker Plugin -->
+	<script src="{{ URL::asset('dashboard/js/bootstrap-selectpicker.js') }}"></script>
+
+	<!--  Checkbox, Radio, Switch and Tags Input Plugins -->
+	<script src="{{ URL::asset('dashboard/js/bootstrap-checkbox-radio-switch-tags.js') }}"></script>
+
+	<!--  Charts Plugin -->
+	<script src="{{ URL::asset('dashboard/js/chartist.min.js') }}"></script>
+
+	<!--  Notifications Plugin    -->
+	<script src="{{ URL::asset('dashboard/js/bootstrap-notify.js') }}"></script>
+
+	<!-- Sweet Alert 2 plugin -->
+	<script src="{{ URL::asset('dashboard/js/sweetalert2.js') }}"></script>
+
+	<!-- Vector Map plugin -->
+	<script src="{{ URL::asset('dashboard/js/jquery-jvectormap.js') }}"></script>
+
+	<!--  Google Maps Plugin    -->
+	<script src="https://maps.googleapis.com/maps/api/js"></script>
+
+	<!-- Wizard Plugin    -->
+	<script src="{{ URL::asset('dashboard/js/jquery.bootstrap.wizard.min.js') }}"></script>
+
+	<!--  Datatable Plugin    -->
+	<script src="{{ URL::asset('dashboard/js/bootstrap-table.js') }}"></script>
+
+	<!--  Full Calendar Plugin    -->
+	<script src="{{ URL::asset('dashboard/js/fullcalendar.min.js') }}"></script>
+
+	<!-- Light Bootstrap Dashboard Core javascript and methods -->
+	<script src="{{ URL::asset('dashboard/js/light-bootstrap-dashboard.js') }}"></script>
+
+	<!--   Sharrre Library    -->
+	<script src="{{ URL::asset('dashboard/js/jquery.sharrre.js') }}"></script>
+
+	<!-- Light Bootstrap Dashboard DEMO methods, don't include it in your project! -->
+	{{--<script src="{{ URL::asset('dashboard/js/demo.js') }}"></script>--}}
+	<script type="text/javascript">
+		$().ready(function(){
+			lbd.checkFullPageBackgroundImage();
+
+			setTimeout(function(){
+				// after 1000 ms we add the class animated to the login/register card
+				$('.card').removeClass('card-hidden');
+			}, 700)
+		});
+	</script>
 </html>
--- resources/views/admin/nonmaster/dashboard_user/login.blade.php
+++ resources/views/admin/nonmaster/dashboard_user/login.blade.php
@@ -1,152 +1,68 @@
 @extends('admin.master.login')
 
 @section('content')
-<body class="signup-page">
+
+	<body>
 	<nav class="navbar navbar-transparent navbar-absolute">
-    	<div class="container">
-        	<!-- Brand and toggle get grouped for better mobile display -->
-        	<div class="navbar-header">
-        		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example">
-            		<span class="sr-only">Toggle navigation</span>
-		            <span class="icon-bar"></span>
-		            <span class="icon-bar"></span>
-		            <span class="icon-bar"></span>
-        		</button>
-        		<a class="navbar-brand" href="{{url('/login')}}">SI-ONENG</a>
-        	</div>
-
-        	<div class="collapse navbar-collapse" id="navigation-example">
-        		<ul class="nav navbar-nav navbar-right">
-					<!--<li>
-    					<a href="../components-documentation.html" target="_blank">
-    						Components
-    					</a>
-    				</li>
-    				<li>
-						<a href="http://demos.creative-tim.com/material-kit-pro/presentation.html?ref=utp-freebie" target="_blank">
-							<i class="material-icons">unarchive</i> Upgrade to PRO
-						</a>
-    				</li>
-		            <li>
-		                <a href="https://twitter.com/CreativeTim" target="_blank" class="btn btn-simple btn-white btn-just-icon">
-							<i class="fa fa-twitter"></i>
-						</a>
-		            </li>
-		            <li>
-		                <a href="https://www.facebook.com/CreativeTim" target="_blank" class="btn btn-simple btn-white btn-just-icon">
-							<i class="fa fa-facebook-square"></i>
-						</a>
-		            </li>
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example-2">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="{{url('/')}}">Si-Oneng</a>
+			</div>
+			<div class="collapse navbar-collapse">
+				<ul class="nav navbar-nav navbar-right">
 					<li>
-		                <a href="https://www.instagram.com/CreativeTimOfficial" target="_blank" class="btn btn-simple btn-white btn-just-icon">
-							<i class="fa fa-instagram"></i>
+						<a {{--href="register.html"--}}>
+							Rekonsiliasi Energi
 						</a>
-		            </li>-->
-        		</ul>
-        	</div>
-    	</div>
-    </nav>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
 
-    <div class="wrapper">
-		<div class="header header-filter" style="background-image: url('{{ URL::asset('material-kit/img/city.jpg') }}'); background-size: cover; background-position: top center;">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
-						<div class="card card-signup">
-							<form class="form" method="" action="">
-								<div class="header header-primary text-center">
-									<h4>Login</h4>
-									<!--<div class="social-line">
-										<a href="#pablo" class="btn btn-simple btn-just-icon">
-											<i class="fa fa-facebook-square"></i>
-										</a>
-										<a href="#pablo" class="btn btn-simple btn-just-icon">
-											<i class="fa fa-twitter"></i>
-										</a>
-										<a href="#pablo" class="btn btn-simple btn-just-icon">
-											<i class="fa fa-google-plus"></i>
-										</a>
-									</div>-->
-								</div>
-								<!--<p class="text-divider">Or Be Classical</p>-->
-								<div class="content">
 
-									<!--<div class="input-group">
-										<span class="input-group-addon">
-											<i class="material-icons">face</i>
-										</span>
-										<input type="text" class="form-control" placeholder="First Name...">
-									</div>-->
+	<div class="wrapper wrapper-full-page">
+		<div class="full-page login-page" data-color="azure" data-image="{{ URL::asset('dashboard/img/full-screen-image-2.jpg') }}">
 
-									<div class="input-group">
-										<span class="input-group-addon">
-											<i class="material-icons">email</i>
-										</span>
-										<input type="text" class="form-control" placeholder="Email...">
+			<!--   you can change the color of the filter page using: data-color="blue | azure | green | orange | red | purple" -->
+			<div class="content">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4 col-sm-6 col-md-offset-4 col-sm-offset-3">
+							<form method="#" action="#">
+
+								<!--   if you want to have the card without animation please remove the ".card-hidden" class   -->
+								<div class="card card-hidden">
+									<div class="header text-center">Login</div>
+									<div class="content">
+										<div class="form-group">
+											<label>Email address</label>
+											<input type="username" placeholder="Username" class="form-control">
+										</div>
+										<div class="form-group">
+											<label>Password</label>
+											<input type="password" placeholder="Password" class="form-control">
+										</div>
 									</div>
-
-									<div class="input-group">
-										<span class="input-group-addon">
-											<i class="material-icons">lock_outline</i>
-										</span>
-										<input type="password" placeholder="Password..." class="form-control" />
+									<div class="footer text-center">
+										<button type="submit" class="btn btn-fill btn-info btn-wd">Login</button>
 									</div>
-
-									<!-- If you want to add a checkbox to this form, uncomment this code
-
-									<div class="checkbox">
-										<label>
-											<input type="checkbox" name="optionsCheckboxes" checked>
-											Subscribe to newsletter
-										</label>
-									</div> -->
 								</div>
-								<div class="footer text-center">
-									<a href="{{url('/')}}" class="btn btn-simple btn-primary btn-lg">Login</a>
-								</div>
+
 							</form>
+
 						</div>
 					</div>
 				</div>
 			</div>
-
-			<footer class="footer">
-		        <div class="container">
-		            <!--<nav class="pull-left">
-						<ul>
-							<li>
-								<a href="http://www.creative-tim.com">
-									Creative Tim
-								</a>
-							</li>
-							<li>
-								<a href="http://presentation.creative-tim.com">
-								   About Us
-								</a>
-							</li>
-							<li>
-								<a href="http://blog.creative-tim.com">
-								   Blog
-								</a>
-							</li>
-							<li>
-								<a href="http://www.creative-tim.com/license">
-									Licenses
-								</a>
-							</li>
-						</ul>
-		            </nav>-->
-		            <div class="copyright pull-right">
-		                &copy; 2016 <a href="#">SI-ONENG<!--</a>--> PT. PLN PERSERO</a>
-		            </div>
-		        </div>
-		    </footer>
-
 		</div>
-
-    </div>
-
-
+	</div>
 </body>
 
 @endsection
