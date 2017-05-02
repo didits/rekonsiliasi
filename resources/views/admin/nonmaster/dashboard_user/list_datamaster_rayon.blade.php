00b52b00c48fa241cc5a9207e7ebedefc8612f17
Danang Adi Nugroho <danang.adi.n@outlook.com>
2017-05-02 21:23:44 +0700

    update view area

--- resources/views/admin/nonmaster/dashboard_user/list_datamaster_rayon.blade.php
+++ resources/views/admin/nonmaster/dashboard_user/list_datamaster_rayon.blade.php
@@ -54,6 +54,78 @@
                             </div>
                         </div>
                     </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card">
+                                <form id="registerFormValidation" action="" method="" novalidate="novalidate">
+                                    <div class="header">Tambah Gardu</div>
+                                    <div class="content">
+
+
+                                        <div class="form-group">
+                                            <label class="control-label">Nama Gardu <star>*</star></label>
+                                            <input class="form-control" name="tambahnamapenyulang" type="text" required="required" aria-required="true">
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label class="control-label">Rayon <star>*</star></label>
+                                            <input class="form-control" name="tambahnamarayon" type="text" required="required" aria-required="true">
+                                        </div>
+
+                                        <div class="form-group">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="btn-group bootstrap-select">
+                                                        <div class="btn-group bootstrap-select">
+                                                            <select name="selectrayonsingle" class="selectpicker" data-title="Single Select" data-style="btn-default btn-block" data-menu-style="dropdown-blue" tabindex="-98">
+                                                                <option class="bs-title-option" value="">Single Select</option>
+                                                                <option value="010101">Rayon Sukolilo</option>
+                                                                <option value="010102">Rayon Gubeng</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="btn-group bootstrap-select show-tick">
+                                                        <div class="btn-group bootstrap-select show-tick">
+                                                            <select multiple="" data-title="Multiple Select" name="selectrayonmultiple" class="selectpicker" data-style="btn-info btn-fill btn-block" data-menu-style="dropdown-blue" tabindex="-98">
+                                                                <option value="010101">Rayon Sukolilo</option>
+                                                                <option value="010102">Rayon Gubeng</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label class="control-label">Jenis Penyulang</label>
+                                            <label class="radio checked">
+                                                                    <span class="icons"><span class="first-icon fa fa-circle-o"></span>
+                                                                    <span class="second-icon fa fa-dot-circle-o"></span></span>
+                                                <input type="radio" data-toggle="radio" name="optionsRadios" value="option1">GD
+                                            </label>
+                                            <div class="clearfix"></div>
+                                            <label class="radio">
+                                                                    <span class="icons"><span class="first-icon fa fa-circle-o"></span>
+                                                                    <span class="second-icon fa fa-dot-circle-o"></span></span>
+                                                <input type="radio" data-toggle="radio" name="optionsRadios" value="option2">PCT
+                                            </label>
+
+                                        </div>
+
+
+                                        <div class="category"><star>*</star> Required fields</div>
+                                    </div>
+
+                                    <div class="footer">
+                                        <button type="submit" class="btn btn-info btn-fill pull-right">Register</button>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
