643866d32b58645bad73f257b84593e1f8c94a0f
ghulamfajri <ghulamfajri13@gmail.com>
2017-08-28 09:53:14 +0700

    Update Struktur

--- app/Http/Controllers/AreaController.php
+++ app/Http/Controllers/AreaController.php
@@ -32,16 +32,16 @@ class AreaController extends Controller
         $area = array('name' => $nama_rayon->nama_organisasi, 'title' => '', 'children' => '', 'office' => 'Rayon ');
         for ($h=0; $h < 1; $h++) { 
             $gi = GI::where('id_organisasi', $id_organisasi)->get(array('nama_gi as name','alamat_gi as title', 'id as id'))->toArray();
-            $gi_array = '';
+            $gi_array = array();
             for ($i=0; $i < count($gi); $i++) { 
                 $trafo_gi = TrafoGI::where('id_organisasi', $id_organisasi)->where('id_gi',$gi[$i]['id'])->get(array('nama_trafo_gi as name','alamat_trafo_gi as title', 'id as id'))->toArray();
-                $trafo_gi_array = '';
+                $trafo_gi_array = array();
                 for ($j=0; $j < count($trafo_gi); $j++) { 
                     $penyulang = Penyulang::where('id_organisasi', $id_organisasi)->where('id_trafo_gi', $trafo_gi[$j]['id'])->get(array('nama_penyulang as name','alamat_penyulang as title', 'id as id'))->toArray();
-                    $penyulang_array = '';
+                    $penyulang_array = array();
                     for ($k=0; $k < count($penyulang); $k++) {
                         $gardu = Gardu::where('id_organisasi', $id_organisasi)->where('id_penyulang', $penyulang[$k]['id'])->get(array('nama_gardu as name','alamat_gardu as title'))->toArray();
-                        $gardu_array = '';
+                        $gardu_array = array();
                         for ($l=0; $l < count($gardu); $l++) { 
                             if($gardu[$l])
                                 $gardu_array[$l] = array('name' => $gardu[$l]['name'], 'title' => $penyulang[$l]['title'], 'office' => 'GD');
@@ -50,19 +50,19 @@ class AreaController extends Controller
                             $penyulang_array_ = array('name' => $penyulang[$k]['name'], 'title' => $penyulang[$k]['title'], 'children' => $gardu_array, 'office' => 'Penyulang');
                         else
                             $penyulang_array_ = array('name' => $penyulang[$k]['name'], 'title' => $penyulang[$k]['title'], 'office' => 'Penyulang');
-                        $penyulang_array[$k] = $penyulang_array_;
+                        array_push($penyulang_array,$penyulang_array_);
                     }
                     if($penyulang_array)
                         $trafo_gi_array_ = array('name' => $trafo_gi[$i]['name'], 'title' => $trafo_gi[$i]['title'], 'children' => $penyulang_array, 'office' => 'Trafo GI');
                     else
                         $trafo_gi_array_ = array('name' => $trafo_gi[$i]['name'], 'title' => $trafo_gi[$i]['title'], 'office' => 'Trafo GI');
-                    $trafo_gi_array[$j] = $trafo_gi_array_;
+                    array_push($trafo_gi_array,$trafo_gi_array_);
                 }
                 if($trafo_gi_array)
                     $gi_array_ = array('name' => $gi[$h]['name'], 'title' => $gi[$h]['title'], 'children' => $trafo_gi_array, 'office' => 'GI');
                 else
                     $gi_array_ = array('name' => $gi[$h]['name'], 'title' => $gi[$h]['title'], 'office' => 'GI');
-                $gi_array[$i] =  $gi_array_;
+                array_push($gi_array,$gi_array_);
             }
             $area['children'] = $gi_array;
         }
