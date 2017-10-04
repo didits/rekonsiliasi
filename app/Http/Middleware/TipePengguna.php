263acceb5ccdd0be919f2d7e4e13680796cf349f
Danang Adi Nugroho <danang13@mhs.if.its.ac.id>
2017-10-04 13:48:33 +0700

    redirect ke forbidden

---
+++ app/Http/Middleware/TipePengguna.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class TipePengguna
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $tipe)
+    {
+        if ($request->user()->tipe_organisasi != $tipe) {
+            return response('Forbidden', '403');
+        }
+        else
+            return $next($request);
+    }
+}
