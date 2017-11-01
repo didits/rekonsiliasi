8c538724a4ed021e3236db68fd36212cbdf2bdae
ghulamfajri <ghulamfajri13@gmail.com>
2017-11-02 06:36:21 +0700

    TSA RAYON

--- app/Http/Controllers/Laporan.php
+++ app/Http/Controllers/Laporan.php
@@ -460,12 +460,12 @@ class Laporan extends Controller
                             $trafo_Kvarh += $kvar;
                             $trafo_KWH += $KWH;
                             $trafo_KWH_lalu += $KWH_bulan_lalu;
-                            $jual = 1000000;
-//                            $jual =   json_decode($penyulang_array[$j]['data_'],true)['jual']['total_kwh_jual'];
-//                            if(json_decode($penyulang_array[$j]['data_'],true)['hasil_pengolahan']['download']['total_pemakaian_kwh_download']==0)
-//                                $ujung =json_decode($penyulang_array[$j]['data_'],true)['beli']['visual']['tu_visual'];
-//                            else $ujung =json_decode($penyulang_array[$j]['data_'],true)['beli']['download']['tu_download'];
-                            $ujung =null;
+//                            $jual = 1000000;
+                            $jual =   json_decode($penyulang_array[$j]['data_'],true)['jual']['total_kwh_jual'];
+                            if(json_decode($penyulang_array[$j]['data_'],true)['hasil_pengolahan']['download']['total_pemakaian_kwh_download']==0)
+                                $ujung =json_decode($penyulang_array[$j]['data_'],true)['beli']['visual']['tu_visual'];
+                            else $ujung =json_decode($penyulang_array[$j]['data_'],true)['beli']['download']['tu_download'];
+//                            $ujung =null;
                             $susut =   $total_kwh-$jual;
                             $trafo_jual += $jual;
                             if($total_kwh==0) $losses=0;
